15368f617c0032a27264ec399aa4c682e3200858
maniel <manuelpepr99@icloud.com>
2025-09-17 13:55:30 +0000

    update

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -3,3 +3,9 @@
 const cpp = new Cpp();
 const java = new Java();
 const react = new React();
+
+export const cTeacher: Teacher = {
+    firstName: "Joe";
+    lastName: "farrah";
+    experienceTeachingC: 10;
+}
